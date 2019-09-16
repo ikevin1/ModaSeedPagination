82e5e4607ca84b421cb12a1cefb82d9bf15b571b
ikevin <ikevin@my.wctc.edu>
2019-09-15 22:58:33 -0700

    Pagination completed

---
+++ Modas/Models/ViewModels/EventListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modas.Models.ViewModels
+{
+    public class EventListViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
